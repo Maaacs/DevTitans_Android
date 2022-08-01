d7543d5c9e47f982b8d37539a79548b8e6b58773
Maaacs <msb@icomp.ufam.edu.br>
2022-08-01 13:20:09 -0400

    App Conta Cliques

--- ContaClique/app/src/main/java/com/example/androidlab02/MainActivity.kt
+++ ContaClique/app/src/main/java/com/example/androidlab02/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private Button botaoIncrementa
+    private TextView displayValor
+    private int acumulador
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
