d95778f35bd206aa4701cbe98a31d85c6af2a8a7
Maaacs <msb@icomp.ufam.edu.br>
2022-08-01 14:36:34 -0400

    Adding attributes

--- ContaCliques/app/src/main/java/com/example/contacliques/MainActivity.java
+++ ContaCliques/app/src/main/java/com/example/contacliques/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.contacliques;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button botaoIncrementa;
+    private TextView displayValor;
+    private int acumulador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public Button getBotaoIncrementa() {
+        return botaoIncrementa;
+    }
+    public void setBotaoIncrementa(Button botaoIncrementa) {
+        this.botaoIncrementa = botaoIncrementa;
+    }
+
+
+    public TextView getDisplayValor() {
+        return displayValor;
+    }
+    public void setDisplayValor(TextView displayValor) {
+        this.displayValor = displayValor;
+    }
+
+
+    public int getAcumulador() {
+        return acumulador;
+    }
+    public void setAcumulador(int acumulador) {
+        this.acumulador = acumulador;
+    }
+
+
+
+
 }
+
+
