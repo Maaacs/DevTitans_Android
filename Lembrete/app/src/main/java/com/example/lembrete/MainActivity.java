86381d4b0600e44696451a031099c8ccc674e157
Max Souza <msb@icomp.ufam.edu.br>
2022-08-02 19:37:27 -0400

    Inicializando 2 Activity

--- Lembrete/app/src/main/java/com/example/lembrete/MainActivity.java
+++ Lembrete/app/src/main/java/com/example/lembrete/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.lembrete;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button BotaoArbrirProximaTela;
+
+
+   public void BotaoAbrirProximaTela(View view){
+       Intent nextActivity = new Intent(MainActivity.this, MainActivity2.class);
+       //Intent nextActivity = new Intent(packageContext:MainActivity.this, MainActivity2.class);
+       startActivity(nextActivity);
+   }
+
+    public Button getBotaoArbrirProximaTela() {
+        return BotaoArbrirProximaTela;
+    }
+
+    public void setBotaoArbrirProximaTela(Button botaoArbrirProximaTela) {
+        BotaoArbrirProximaTela = botaoArbrirProximaTela;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*this.setDisplayEntrada(findViewById(R.id));
+        this.setEntradaTexto(findViewByIdr.id());
+        this.setSalvarBotao(findViewById())*/
+        this.setBotaoArbrirProximaTela(findViewById(R.id.button));
+
+        /*this.setTextRecebido(findViewById(R.id));
+        Intent intentRecebido = getIntent();
+        getTextRecebido().setText(intentRecebido.getStringExtra());*/
     }
 }
